3e65ea670443a0b2943d8f0dc3dd4782763e0768
Ilmangamer <73790983+Ilmangamer@users.noreply.github.com>
2021-12-08 18:23:06 +0100

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from datetime import datetime, timedelta
 
-
-
+# ####--- Note to developer---- #####
 # Get a clock in tkinter GUI
 # Imports 
 # Size and geometry
@@ -11,18 +10,15 @@ from datetime import datetime, timedelta
 # Configure that variable to your existing label
 # tell it to display after 1000 ms, then show time
 
-
-
-
 root = tk.Tk()
 root.geometry("250x100")
 
 
 def moving_time():
     now = datetime.now()
-    a = (now.strftime("%H:%M:%S %p"))
-    ab = (now + timedelta(seconds =+1)).strftime("%H:%M:%S %p")
-    return ab
+    now_strftime = (now.strftime("%H:%M:%S %p"))
+    now_timedelta = (now + timedelta(seconds =+1)).strftime("%H:%M:%S %p")
+    return now_timedelta
 
 clock_label = tk.Label(root, text= moving_time(), font=("ds-digital", 80), background="black", foreground="green")
 clock_label.pack(anchor="center")
@@ -36,15 +32,18 @@ def update():
 
 update()
 
+root.mainloop()
+
+#####---- Note to developer #########
 # tkinter way of updating variables
 """"
 clock_label["text"] = moving_time()
-What it does is that it updates the text variable objec to what the moving_time function returns
+What it does is that it updates the text variable object to what the moving_time function returns
 """
 # normal way of updating variables
 """
 my_obeject.my_value = new_value
 """
-root.mainloop()
+
 
 
